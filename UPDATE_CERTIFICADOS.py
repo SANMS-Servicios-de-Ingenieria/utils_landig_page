66cdaa56ac1538f4e363bf637fbd8babd1c0103d
jcaramantin <jcaramantin@sanms.com.pe>
2022-09-06 14:11:05 -0500

    feat: Version for SANMS Repository

--- UPDATE_CERTIFICADOS.py
+++ UPDATE_CERTIFICADOS.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-engine = create_engine( )
+engine = create_engine("mysql+pymysql://" + "sanmscom_wp316" + ":" + "6upS4Sr3]" + "@" + "69.167.175.221" + ":" + "3306" + "/" + "sanmscom_wp316" + "?" + "charset=utf8mb4")
 
 conn = engine.connect()
 
@@ -14,16 +14,25 @@ def UploadExcel(archivonombre,tabla):
     query="ALTER TABLE `{table}` ADD PRIMARY KEY (`ID`);".format(table=tabla)
     query2="ALTER TABLE `{table}`CHANGE COLUMN `ID` `ID` BIGINT(20) NOT NULL AUTO_INCREMENT FIRST;".format(table=tabla)
     conn.execute(query)
+    conn.execute(query2)
 
 def GetExcel(urldoc,ubicación):
     gsread=pd.read_csv(urldoc,index_col=0)
     gsread.to_excel(ubicación)
     
 
-GetExcel('',"utils/CERTIFICADOS.xlsx")
-GetExcel('',"utils/Productos.xlsx")
-GetExcel('',"utils/Noticias.xlsx")
-GetExcel('',"utils/Proveedores.xlsx")
+GetExcel('https://docs.google.com/spreadsheets/d/' + 
+                   '1VobD0chAEG5QsMxgiSvcEj5v2q_QqWoKcuHzP9frQFo' +
+                   '/export?gid=0&format=csv',"utils/CERTIFICADOS.xlsx")
+GetExcel('https://docs.google.com/spreadsheets/d/' + 
+                   '1VobD0chAEG5QsMxgiSvcEj5v2q_QqWoKcuHzP9frQFo' +
+                   '/export?gid=525308165&format=csv',"utils/Productos.xlsx")
+GetExcel('https://docs.google.com/spreadsheets/d/' + 
+                   '1VobD0chAEG5QsMxgiSvcEj5v2q_QqWoKcuHzP9frQFo' +
+                   '/export?gid=1151957110&format=csv',"utils/Noticias.xlsx")
+GetExcel('https://docs.google.com/spreadsheets/d/' + 
+                   '1VobD0chAEG5QsMxgiSvcEj5v2q_QqWoKcuHzP9frQFo' +
+                   '/export?gid=1385796145&format=csv',"utils/Proveedores.xlsx")
 
 
 UploadExcel('utils/CERTIFICADOS.xlsx',"Usuarios")
